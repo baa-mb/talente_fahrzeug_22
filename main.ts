803d514ad7929ffe72a98a4d2f83ea1a447e2d78
baa <34665120+baa-mb@users.noreply.github.com>
2026-06-06 15:09:33 +0200

    Fertig juni

--- main.ts
+++ main.ts
@@ -1,41 +1,17 @@
-input.onButtonPressed(Button.A, function () {
-    lauf_flag = 1
-})
-// if (info == "TB") {
-// if (wert == 1) {
-// control.raiseEvent(
-// EventBusSource.MICROBIT_ID_BUTTON_B,
-// EventBusValue.MICROBIT_BUTTON_EVT_CLICK
-// )
-// } else {
-// control.raiseEvent(
-// EventBusSource.MICROBIT_ID_BUTTON_A,
-// EventBusValue.MICROBIT_BUTTON_EVT_CLICK
-// )
-// }
-// }
 radio.onReceivedValue(function (info, wert) {
-    serial.writeLine("gerade: " + gerade_get + " kurve: " + kurve_get)
     if (info == "kurve") {
         kurve_get = wert
         kurve_rad = Math.round(Math.map(kurve_get, -45, 45, -255, 255))
     } else if (info == "gerade") {
         gerade_get = wert
         gerade_rad = Math.round(Math.map(gerade_get, -45, 45, -255, 255))
+    } else if (info == "kupplung") {
+        if (wert == 0) {
+            robotbit.Servo(robotbit.Servos.S1, 0)
+        } else {
+            robotbit.Servo(robotbit.Servos.S1, heben_winkel)
+        }
     }
-})
-input.onButtonPressed(Button.AB, function () {
-    for (let index = 0; index < 4; index++) {
-        robotbit.Servo(robotbit.Servos.S1, 0)
-        robotbit.Servo(robotbit.Servos.S2, 0)
-        basic.pause(1000)
-        robotbit.Servo(robotbit.Servos.S1, 60)
-        robotbit.Servo(robotbit.Servos.S2, 180)
-        basic.pause(2000)
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    lauf_flag = 0
 })
 let rechts_soll = 0
 let links_soll = 0
@@ -44,22 +20,24 @@ let kurve_links = 0
 let gerade_rechts = 0
 let gerade_links = 0
 let gerade_rad = 0
+let gerade_get = 0
 let kurve_rad = 0
 let kurve_get = 0
-let gerade_get = 0
+let heben_winkel = 0
+heben_winkel = 70
 let lauf_flag = 0
 let links_rad = 0
 let rechts_rad = 0
 let links_ist = 0
 let rechts_ist = 0
 radio.setGroup(26)
-lauf_flag = 0
 basic.showIcon(IconNames.Diamond)
 let motor_links = robotbit.Motors.M1A
 let motor_rechts = robotbit.Motors.M2A
 robotbit.MotorStopAll()
 let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 strip.showRainbow(1, 360)
+robotbit.Servo(robotbit.Servos.S1, 0)
 basic.forever(function () {
     gerade_links = gerade_rad * 1.2
     gerade_rechts = gerade_rad
